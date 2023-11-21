35a0ec12fdcbf2c82f5eafc125f82f6b29a930bb
Dylan <yourmom12734@gmail.com>
2023-11-20 20:59:46 -0500

    added Required

--- restaurantfavesBackend/Models/Order.cs
+++ restaurantfavesBackend/Models/Order.cs
@@ -6,7 +6,9 @@ namespace restaurantfavesBackend.Models
     {
         [Key]
         public int Id { get; set; }
+        [Required]
         public string Description { get; set; }
+        [Required]
         public string Restaurant { get; set; }
         public int Rating { get; set; }
         public bool OrderAgain { get; set; }
