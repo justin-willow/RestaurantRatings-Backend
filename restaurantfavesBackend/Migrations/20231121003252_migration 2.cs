70d09424d25a416174b33e2a8ba38f17bb1032c2
Dylan <yourmom12734@gmail.com>
2023-11-20 20:15:27 -0500

    commit 2

---
+++ restaurantfavesBackend/Migrations/20231121003252_migration 2.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace restaurantfavesBackend.Migrations
+{
+    public partial class migration2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
